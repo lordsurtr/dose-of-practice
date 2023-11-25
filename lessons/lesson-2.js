e773bc2397990118727aa2287028e6b2cbcdc31a
lordsurtr <kresan.bohdan@gmail.com>
2023-11-25 12:08:38 +0200

    add more words to the second lesson

--- lessons/lesson-2.js
+++ lessons/lesson-2.js
@@ -23,7 +23,38 @@ const lesson = new Lesson('Урок 1', [
         type: "write",
         word: "крісло",
         correctAnswer: "chair"
-    }
+    },
+    {
+        type: "match",
+        word: "annihilation",
+        options: [
+            "анігіляція",
+            "потонути",
+            "знищити",
+            "відшити"
+        ],
+        correctAnswer: "анігіляція"
+    },
+    {
+        type: "write",
+        word: "екран",
+        correctAnswer: "screen"
+    },
+    {
+        type: "write",
+        word: "курка",
+        correctAnswer: "chicken"
+    },
+    {
+        type: "write",
+        word: "мишка",
+        correctAnswer: "mouse"
+    },
+    {
+        type: "write",
+        word: "жирний",
+        correctAnswer: "fat"
+    },
 ])
 
 export default lesson
